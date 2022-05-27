bff785b425ac5f3fe02b10efcc878490b0f8b1fb
Panu Valtanen <p4nu@users.noreply.github.com>
2022-05-27 07:20:35 +0000

    Add functionality to route

--- routes/asteroids.js
+++ routes/asteroids.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 require('dotenv').config();
 
 /* GET asteroids listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  try {
+    const { data } = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.start_date}&end_date=${req.query.end_date}&api_key=${process.env.NASA_API_KEY}`);
+    res.json(data);
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
 });
 
 module.exports = router;
