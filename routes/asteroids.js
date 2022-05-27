b8d507fda3ce92dadc6ae69c47ccc97998370f8f
Panu Valtanen <p4nu@users.noreply.github.com>
2022-05-27 07:33:35 +0000

    Use destructuring in error message

--- routes/asteroids.js
+++ routes/asteroids.js
@@ -8,8 +8,8 @@ router.get('/', async (req, res) => {
   try {
     const { data } = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${req.query.start_date}&end_date=${req.query.end_date}&api_key=${process.env.NASA_API_KEY}`);
     res.json(data);
-  } catch (e) {
-    res.status(500).json({ message: e.message });
+  } catch ({ message }) {
+    res.status(500).json(message);
   }
 });
 
